54b34e7c5ad78e7e6543f4bcf790406c68d5bd9d
John-Marc Chandonia <jmchandonia@lbl.gov>
2020-10-01 14:23:16 -0700

    More renaming of spades to unicycler

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,4 +2,4 @@
 script_dir=$(dirname "$(readlink -f "$0")")
 export KB_DEPLOYMENT_CONFIG=$script_dir/../deploy.cfg
 export PYTHONPATH=$script_dir/../lib:$PATH:$PYTHONPATH
-uwsgi --master --processes 5 --threads 5 --http :5000 --wsgi-file $script_dir/../lib/kb_SPAdes/kb_SPAdesServer.py
+uwsgi --master --processes 5 --threads 5 --http :5000 --wsgi-file $script_dir/../lib/kb_unicycler/kb_unicyclerServer.py
